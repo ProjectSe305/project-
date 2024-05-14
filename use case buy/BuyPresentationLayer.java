800d70ee90ea37c579c0a3f71f88e23afb14f401
MuntRah <161588122+MuntRah@users.noreply.github.com>
2024-05-14 22:39:22 +0300

    Update BuyPresentationLayer.java

--- use case buy/BuyPresentationLayer.java
+++ use case buy/BuyPresentationLayer.java
@@ -24,7 +24,7 @@ public class BuyPresentationLayer {
         String inputBookName = scanner.nextLine();
 
         // Call the business layer to process the payment
-        businessLayer.processPayment(price , name);
+        businessLayer.processPayment(price , name , total);
 
         scanner.close();
     }
